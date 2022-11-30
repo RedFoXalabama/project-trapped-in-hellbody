7b0c13bc894c024b7a6eed85aaf63d7c0a9c80a4
RedFoXalabama <90854992+RedFoXalabama@users.noreply.github.com>
2022-11-30 18:47:58 +0100

    :Hammer: PopupMenu Combat system
    Ho sistemato l'errore creatosi nel precedente commit

--- Combat System/Demo Fight/Demo_Fight.cs
+++ Combat System/Demo Fight/Demo_Fight.cs
@@ -1,14 +1,11 @@
 using Godot;
 using System;
 
-public class Demo_Fight : Node
+public class Demo_Fight : Node2D
 {
-    public override void _Ready()
-    {
-        var combat_menu = GetNode<PopupMenu>("Node2D/PopupMenu");
+    public override void _Ready(){
+        var combat_menu = GetNode<PopupMenu>("PopupMenu");
         var combat_menu_Rect = new Rect2(combat_menu.RectPosition, combat_menu.RectSize);
         combat_menu.Popup_(combat_menu_Rect);
-    }   
-
-
+    }
 }
