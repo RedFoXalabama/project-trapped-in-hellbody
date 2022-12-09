fdb8b9d831d4074de6029ec91498a3e725aa1a4d
RedFoXalabama <redfoxalabama.art@gmail.com>
2022-12-10 00:05:06 +0100

    :bento: :hammer: Creazione *_BattleAnimation e importazione Grafiche temp
    Ho lavorato su Godot implementando le grafiche temporanee, importate con l’impostazione Pixel 2D e ho creato le scene *_BattleAnimation

--- Combat System/Demo Fight/Demo_Fight.cs
+++ Combat System/Demo Fight/Demo_Fight.cs
@@ -3,34 +3,34 @@ using System;
 
 public class Demo_Fight : Node2D
 {   
-    private Boolean is_focused;
-    private int id_selected;
-    public override void _Ready(){
-        var combat_menu = GetNode<PopupMenu>("PopupMenu");
-        var combat_menu_Rect = new Rect2(combat_menu.RectPosition, combat_menu.RectSize);
-        combat_menu.Popup_(combat_menu_Rect);
-    }
-    public override void _Process(float delta){
-        var combat_menu = GetNode<PopupMenu>("PopupMenu");
-        var combat_menu_Rect = new Rect2(combat_menu.RectPosition, combat_menu.RectSize);
-        if (combat_menu.Visible){
-            if(is_focused && Input.IsActionJustPressed("ui_selection")){
-                switch(id_selected){
-                    case 0:
-                    is_focused = false;
-                    var skill_PopupMenu = GetNode<PopupMenu>("PopupMenu/Skill_PopupMenu");
-                    var skill_PopupMenu_Rect = new Rect2(skill_PopupMenu.RectPosition, skill_PopupMenu.RectSize);
-                    skill_PopupMenu_Rect.Position += combat_menu_Rect.Position;
-                    //skill_PopupMenu.y += combat_menu_Rect.y;
-                    skill_PopupMenu.Popup_(skill_PopupMenu_Rect);
-                    break;
-                }
-            }
-        }
-    }
+	private Boolean is_focused;
+	private int id_selected;
+	public override void _Ready(){
+		var combat_menu = GetNode<PopupMenu>("PopupMenu");
+		var combat_menu_Rect = new Rect2(combat_menu.RectPosition, combat_menu.RectSize);
+		combat_menu.Popup_(combat_menu_Rect);
+	}
+	public override void _Process(float delta){
+		var combat_menu = GetNode<PopupMenu>("PopupMenu");
+		var combat_menu_Rect = new Rect2(combat_menu.RectPosition, combat_menu.RectSize);
+		if (combat_menu.Visible){
+			if(is_focused && Input.IsActionJustPressed("ui_selection")){
+				switch(id_selected){
+					case 0:
+					is_focused = false;
+					var skill_PopupMenu = GetNode<PopupMenu>("PopupMenu/Skill_PopupMenu");
+					var skill_PopupMenu_Rect = new Rect2(skill_PopupMenu.RectPosition, skill_PopupMenu.RectSize);
+					skill_PopupMenu_Rect.Position += combat_menu_Rect.Position;
+					//skill_PopupMenu.y += combat_menu_Rect.y;
+					skill_PopupMenu.Popup_(skill_PopupMenu_Rect);
+					break;
+				}
+			}
+		}
+	}
 
-    public void _on_PopupMenu_id_focused(int id){
-       is_focused = true;
-       id_selected = id;
-    }
+	public void _on_PopupMenu_id_focused(int id){
+	   is_focused = true;
+	   id_selected = id;
+	}
 }
